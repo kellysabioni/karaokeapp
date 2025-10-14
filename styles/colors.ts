3cd4b8dabf0e770c56a373ed1109797ee9343c88
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-14 11:59:21 -0300

    Padroniza estilos

---
+++ styles/colors.ts
@@ -0,0 +1,15 @@
+// src/styles/colors.js
+export const colors = {
+  primariaClara: "#4E2A57",
+  primaria: "#36173D",
+  primariaEscura: "#201124",
+  bg_escura: "#1c1433",
+  secundaria: "#FFC55F",
+  secundariaEscura: "#FFC107",
+  textoPrincipal: "#121212",
+  textoSecundario: "#404040",
+  textoIntermediario: "#262626",
+  white: "#Fafafa",
+  alerta: "#DC3545",
+  alerta2: "#FF4845",
+};
