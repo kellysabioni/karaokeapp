6381f9cbf6edee7c265930718d8638da58913d13
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-15 21:59:34 -0300

    Cria api fake de musicas

---
+++ services/apiMusica.ts
@@ -0,0 +1,24 @@
+export default function apiMusica() {
+  return [
+    {
+      id: 1,
+      titulo: "Se...",
+      cantor: "Djavan",
+      capa: require("../assets/images/djavan-foto.png"),
+    },
+
+    {
+      id: 2,
+      titulo: "Velha infância",
+      cantor: "Tribalistas",
+      capa: require("../assets/images/tribalistas.png"),
+    },
+
+    {
+      id: 3,
+      titulo: "Anjos",
+      cantor: "O Rappa",
+      capa: require("../assets/images/orappa.png"),
+    },
+  ];
+}
